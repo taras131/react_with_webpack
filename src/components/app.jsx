e06b3a109dd357f8acac0761076ae1977bb842c2
taras131 <mossnabitkana@gmail.com>
2021-11-11 16:13:37 +0300

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            its work
+        </div>
+    );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'React with Webpack and Babel';
+
+ReactDOM.render(
+    <div>ывпывпывп</div>,
+    document.getElementById('app')
+);
+
